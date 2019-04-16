1aff6388a75b24ce499444a543d6296aaf41371a
ndudnicz <ndudnicz@protonmail.com>
2019-04-16 14:15:25 +0200

    add docs

--- spec/selenite_spec.cr
+++ spec/selenite_spec.cr
@@ -5,8 +5,8 @@ require "spec"
 describe Selenite do
   # TODO: Write tests
 
-  it "10000 tests" do
-    csv = CSVProcessor::CSVP.new("./spec/output.csv")
+  it "hsv_to_rgb 10000 tests" do
+    csv = CSVProcessor::CSVP.new("./spec/files/hsv_to_rgb.csv")
     csv.read_file
     csv.records.each do |row|
       ret = Selenite.hsv_to_rgb(Float64.new(row["h"]), Float64.new(row["s"]), Float64.new(row["v"]))
