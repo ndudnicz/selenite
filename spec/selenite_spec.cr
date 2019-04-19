2aac95b6f84e69d5569867376c585c9962384e21
ndudnicz <ndudnicz@protonmail.com>
2019-04-19 14:13:21 +0200

    add rgb to hsv static method

--- spec/selenite_spec.cr
+++ spec/selenite_spec.cr
@@ -2,6 +2,10 @@ require "../src/selenite"
 require "csv_processor"
 require "spec"
 
+def _round(x : Float64)
+  return x % 1 > 0.5 ? x.ceil : x.floor
+end
+
 describe Selenite do
   # TODO: Write tests
 
@@ -13,4 +17,21 @@ describe Selenite do
       ret.should eq({Float64.new(row["r"]), Float64.new(row["g"]), Float64.new(row["b"])})
     end
   end
+
+  it "rgb_to_hsv 10000 tests" do
+    csv = CSVProcessor::CSVP.new("./spec/files/rgb_to_hsv.csv")
+    csv.read_file
+    csv.records.each do |row|
+      ret = Selenite.rgb_to_hsv(Float64.new(row["r"]), Float64.new(row["g"]), Float64.new(row["b"]))
+      # TODO: round problem handling, feel free to provide better rgb_to_hsv.csv dataset
+      v0 = ret[0] - Float64.new(row["h"])
+      v1 = ret[1] - Float64.new(row["s"])
+      v2 = ret[2] - Float64.new(row["v"])
+      (v0 < 1 && v0 > -1).should eq true
+      (v1 < 1 && v1 > -1).should eq true
+      (v2 < 1 && v2 > -1).should eq true
+      # ret.should eq({Float64.new(row["h"]), Float64.new(row["s"]), Float64.new(row["v"])})
+    end
+  end
+
 end
--- src/selenite.cr
+++ src/selenite.cr
@@ -51,11 +51,50 @@ struct Selenite
   # Selenite.hsv_to_rgb({42.0, 42.0, 42.0}) # => {107.0, 94.0, 62.0}
   # ```
   def self.hsv_to_rgb(
-    hsv : Tuple(Float64, Float64, Float64)
+    _hsv : Tuple(Float64, Float64, Float64)
   ) : Tuple(Float64, Float64, Float64)
-    return self.hsv_to_rgb( hsv[0], hsv[1], hsv[2] )
+    return self.hsv_to_rgb( _hsv[0], _hsv[1], _hsv[2] )
   end
 
-  def rgb_to_hsv()
+  # Returns a hsv tuple Tuple(Float64, Float64, Float64)
+  #
+  # ```
+  # Selenite.rgb_to_hsv(10.0, 22.0, 33.0) # => {208.69565217391306, 69.6969696969697, 12.941176470588237}
+  # ```
+  def self.rgb_to_hsv(
+    _r : Float64,
+    _g : Float64,
+    _b : Float64
+  ) : Tuple(Float64, Float64, Float64)
+    rr = _r / 255
+    gg = _g / 255
+    bb = _b / 255
+    sorted = [rr, gg, bb].sort
+    cmax = sorted[-1]
+    cmin = sorted[0]
+    v = cmax - cmin
+    if v == 0.0
+      h = 0.0
+    elsif cmax == rr
+      h = (((gg - bb) / v) % 6.0) * 60.0
+    elsif cmax == gg
+      h = (((bb - rr) / v) + 2.0) * 60.0
+    else
+      h = (((rr - gg) / v) + 4.0) * 60.0
+    end
+    s = cmax == 0.0 ? 0.0 : v / cmax
+    return {h, s * 100.0, cmax * 100.0}
   end
+
+  # Returns a hsv tuple Tuple(Float64, Float64, Float64)
+  #
+  # ```
+  # Selenite.rgb_to_hsv({42.0, 42.0, 42.0}) # => {107.0, 94.0, 62.0}
+  # ```
+  def self.rgb_to_hsv(
+    _rgb : Tuple(Float64, Float64, Float64)
+  ) : Tuple(Float64, Float64, Float64)
+    return self.rgb_to_hsv( _rgb[0], _rgb[1], _rgb[2] )
+  end
+
 end
